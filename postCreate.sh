49347b4c9491e82a31b08c86ac3ff3a2c7e063cc
Anand Sawant <anandsaw@gmail.com>
2025-02-27 00:19:32 +0000

    Auto create venv

--- postCreate.sh
+++ postCreate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 curl -LO https://github.com/google/osv-scanner/releases/download/v1.9.2/osv-scanner_linux_amd64
 mv osv-scanner_linux_amd64 osv-scanner
 chmod +x osv-scanner
